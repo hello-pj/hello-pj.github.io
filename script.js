eed0feca996411acc2c83d707c3179703f04340d
aimi-p <69302785+Akub1219@users.noreply.github.com>
2025-02-21 22:33:32 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+  var eventTitle = document.getElementById('event-title');
+  var eventDate = document.getElementById('event-date');
+  var eventLocation = document.getElementById('event-location');
+  var eventDescription = document.getElementById('event-description');
+  var eventGroup = document.getElementById('event-group');
+
+  function fetchEventData() {
+    return fetch('https://script.google.com/macros/s/AKfycbxXh9UQzHzgSAxUg8sxAINgapf-XZl-2mIKjbzR0JGqzscrIjBRaG72wgE2MmnQolsKpg/exec')
+      .then(response => response.json())
+      .then(events => {
+        return events.map(event => {
+          let utcDate = new Date(event['Start Date']);
+          let jstDate = new Date(utcDate.getTime() + 9 * 60 * 60 * 1000);
+          event['Start Date'] = jstDate.toISOString().split('T')[0];
+          event['Start Time'] = event['Start Time'].split('T')[1];
+          return {
+            title: event.Subject,
+            start: event['Start Date'] + "T" + event['Start Time'],
+            location: event.Location,
+            description: event.Description,
+            group: event.Group
+          };
+        });
+      });
+  }
+
+  fetchEventData().then(events => {
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      events: events,
+      eventClick: function(info) {
+        eventTitle.textContent = info.event.title;
+        eventDate.textContent = info.event.start.toISOString().split('T')[0];
+        eventLocation.textContent = info.event.extendedProps.location;
+        eventDescription.textContent = info.event.extendedProps.description;
+        eventGroup.textContent = info.event.extendedProps.group;
+      }
+    });
+    calendar.render();
+  });
+});
